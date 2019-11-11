23ffdab3ad228f5004079e37364914c4a166bce0
Mikhail Topal <wallguard313@gmail.com>
2019-11-11 16:19:08 +0300

    many things at once

--- src/js/maxsumm.js
+++ src/js/maxsumm.js
@@ -4,8 +4,7 @@ function maxSummArray(arr) {
     if ((Number.isInteger(arr[0])) && (Number.isInteger(arr[1]))) {
         maxSumm = arr[0] + arr[1];
         sum = arr[0] + arr[1];
-    }
-    else {
+    } else {
         console.log('false')
         return false
     }
@@ -14,12 +13,10 @@ function maxSummArray(arr) {
         if ((arr[i] ^ 0) !== arr[i]){
             console.log('false');
             return false;
-        }
-        else if (maxSumm < sum + arr[i]) {
+        } else if (maxSumm < sum + arr[i]) {
             maxSumm = sum + arr[i];
             sum = sum + arr[i];
-        }
-        else {
+        } else {
             sum = sum + arr[i];
         }
     }
@@ -28,3 +25,25 @@ function maxSummArray(arr) {
 }
 
 maxSummArray([5, 5, 5, 5, 5, 5, 5, 5])
+
+///////////
+function maxSummArray2 (arr) {
+    let maxSum = 0
+    let sum = 0
+    let lastItem = null
+    arr.forEach((item) => {
+      if (lastItem && item === lastItem + 1) {
+        sum = sum > 0 ? sum + item : item + lastItem;
+        lastItem = item;
+        if (sum > maxSum) {
+          maxSum = sum;
+        }
+      } else {
+        lastItem = item;
+      }
+    });
+    console.log(maxSum);
+    return maxSum;
+  }
+
+  maxSummArray2([5, 5, 5, 5, 5, 1, 5, 5])
--- src/js/task3.js
+++ src/js/task3.js
@@ -1,14 +1,7 @@
 function checkBlock(a, b) {
-    if ((a === '') || (b === '')) {
-        return ('Пустые строки!');
-    }
-    else if (a.length !== b.length) {
-        return ('Разная длинна строк!');
-    }
-    else if (a === b) {
-        return ('Одинаковые строки!');
-    }
-    else {
+    if ((a === '') || (b === '') || (a.length !== b.length) || (a === b)) {
+        return false;
+    } else {
         console.log(mf(a, b))
         return mf(a, b);
     }
@@ -18,7 +11,7 @@ mf = function mainFunction(a, b) {
     for (let i = 0, j = 50; i < a.length;) {
         if (a === b) {
             console.log('yes');
-            return ('yes')
+            return true
         }
         else if (j > a.length - 1) {
             i++;
@@ -31,7 +24,7 @@ mf = function mainFunction(a, b) {
         }
         else if ( ((a[i] !== b[j]) && (j > a.length - 1)) ) {
             console.log('No-o-o-o-o');
-            return ('No')
+            return false
         }
         else {
             console.log(0)
@@ -42,3 +35,34 @@ mf = function mainFunction(a, b) {
 }
 
 mf('abcd', 'cbad')
+
+/////////////////////////////////////////////
+
+function validationOfStringConversion(a, b) {
+    if (a.length !== b.length) {
+      return false;
+    }else {
+      let res = true;
+      a.split('').forEach((item, index) => {
+        let firstSt = 0;
+        let secondSt = 0;
+        let pos = -1;
+        while ((pos = a.indexOf(item, pos + 1)) !== -1) {
+          if (pos >= 0) {
+            firstSt = firstSt + (pos & 1) + 1
+          }
+        }
+        while ((pos = b.indexOf(item, pos + 1)) !== -1) {
+          if (pos >= 0) {
+            secondSt = secondSt + (pos & 1) + 1
+          }
+        }
+        if (firstSt !== secondSt) {
+          res = false
+        }
+        firstSt = 0;
+        secondSt = 0;
+      });
+      return res;
+    }
+  }
