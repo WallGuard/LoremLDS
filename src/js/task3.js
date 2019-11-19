7992a701a472af690be19080c41f82a0b0d22372
Mikhail Topal <wallguard313@gmail.com>
2019-11-19 09:26:03 +0300

    task3 ref

--- src/js/task3.js
+++ src/js/task3.js
@@ -3,8 +3,8 @@ function equalString(a, b) {
     console.log('false');
     return false
   } else {
-    console.log(mfn(a, b))
-    return mfn(a, b)
+    console.log(mainFunction2(a, b))
+    return mainFunction2(a, b)
   }
 }
 
@@ -12,27 +12,27 @@ function checkBlock(a, b) {
   return !((a === '') || (b === '') || (a.length !== b.length) || (a === b))
 }
 
-const mfn = function mainFunction2(a, b) {
+function mainFunction2(a, b) {
   a = a.split('');
   b = b.split('');
   for (let i = 0, j = 0; i < a.length;) {
-      if (a.every((value, index) => value === b[index])) {
-          console.log('yes');
-          return true
-      } else if (j > a.length - 1) {
-          i++;
-          j = i;
-      } else if (a[i] === b[j]) {
-          b[i] = a[i];
-          b[j] = a[j];
-          i++;
-          j = i;
-      } else if ((j > b.length - 2) && (j === i))  {
-          console.log('No');
-          return false
-      } else {
-          j += 2;
-      }
+    if (a.every((value, index) => value === b[index])) {
+      console.log('yes');
+      return true
+    } else if (j > a.length - 1) {
+      i++;
+      j = i;
+    } else if (a[i] === b[j]) {
+      b[i] = a[i];
+      b[j] = a[j];
+      i++;
+      j = i;
+    } else if ((j > b.length - 2) && (j === i)) {
+      console.log('No');
+      return false
+    } else {
+      j += 2;
+    }
   }
 }
 
